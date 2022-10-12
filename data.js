da4e253dd74fec63f081a9f01676b4204feda572
Alexandre Trieste <alexandre.trieste@gmail.com>
2022-10-11 21:22:40 -0300

    Feat: Persitence layer

---
+++ data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    id: uuidv4(),
+    boardName: "ToDo",
+    card: [
+      {
+        id: uuidv4(),
+        task: "Create new components",
+        tags: [
+          {
+            tagName: "Design System",
+            color: "#D7F3E4",
+          },
+        ],
+      },
+    ],
+  },
+];
+export default data;
